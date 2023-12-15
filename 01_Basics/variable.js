86147d0a2eeb310cc353dec9d1b2e519935fcf6f
Mukul Rana <104504259+mukulrana21@users.noreply.github.com>
2023-12-15 06:50:11 +0000

    Day1 Js

---
+++ 01_Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 1444321
+let emailId = "mukul@google.com"
+accountAddress = 'Moradabad'
+
+//accountId = 12121212 // we can't change the value "Const" variable once it's defined
+
+/*
+ Don't Ever Use "var", because it has function and block scope issues,
+ Always use Let and Var
+*/
+
+console.log({accountId,emailId, accountAddress})
